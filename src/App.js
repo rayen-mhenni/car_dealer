a9a1963f40ceb32bd2891c8a2f3313eff217dae2
rayen-mhenni <mhennirayen2@gmail.com>
2023-10-02 13:17:37 +0100

    header footer app

--- src/App.js
+++ src/App.js
@@ -7,12 +7,77 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+      <div class="sidebar-menu-container" id="sidebar-menu-container">
+        <div class="sidebar-menu-push">
+          <div class="sidebar-menu-overlay"></div>
 
-        </Routes>
-      </BrowserRouter>
+          <div class="sidebar-menu-inner">
+            <BrowserRouter>
+              <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+            </BrowserRouter>
+          </div>
+        </div>
+
+        <nav class="sidebar-menu slide-from-left">
+          <div class="nano">
+            <div class="content">
+              <nav class="responsive-menu">
+                <ul>
+                  <li>
+                    <a href="index.html">Home</a>
+                  </li>
+                  <li>
+                    <a href="about.html">About Us</a>
+                  </li>
+                  <li>
+                    <a href="services.html">Services</a>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="#">Listing</a>
+                    <ul class="sub-menu">
+                      <li>
+                        <a href="listing-right.html">Sidebar Right</a>
+                      </li>
+                      <li>
+                        <a href="listing-left.html">Sidebar Left</a>
+                      </li>
+                      <li>
+                        <a href="listing-grid.html">Grids System</a>
+                      </li>
+                      <li>
+                        <a href="single-list.html">Car Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="#">Blog</a>
+                    <ul class="sub-menu">
+                      <li>
+                        <a href="blog-right.html">Classic</a>
+                      </li>
+                      <li>
+                        <a href="blog-grid.html">Grids System</a>
+                      </li>
+                      <li>
+                        <a href="grid-right.html">Grids Sidebar</a>
+                      </li>
+                      <li>
+                        <a href="single-blog.html">Single Post</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="about.html">About</a>
+                  </li>
+                  <li>
+                    <a href="contact.html">Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,231 @@
+import axios from "axios";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="about-us">
+                <img src="assets/images/logo-2.png" alt="" />
+                <p>
+                  Maecenas ne mollis orci. Phasell iacu sapie non aliquet ex
+                  euismo ac.
+                </p>
+                <ul>
+                  <li>
+                    <i className="fa fa-map-marker" />
+                    Raver Croft Drive Knoxville, 37921
+                  </li>
+                  <li>
+                    <i className="fa fa-phone" />
+                    +55 417-634-7071
+                  </li>
+                  <li>
+                    <i className="fa fa-envelope-o" />
+                    contact@auction.com
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="featured-links">
+                <h4>Featured Links</h4>
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Term &amp; Services
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Meet The Team
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Company News
+                    </a>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Shop
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      New Vehicle
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Features
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Promotions
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-caret-right" />
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="latest-news">
+                <h4>Latest News</h4>
+                <div className="latest-item">
+                  <img
+                    src="http://dummyimage.com/64x64/cccccc/fff.jpg"
+                    alt=""
+                  />
+                  <a href="single-blog.html">
+                    <h6>Hella Kogi Whatever</h6>
+                  </a>
+                  <ul>
+                    <li>24 Sep,2015</li>
+                    <li>2 comments</li>
+                  </ul>
+                </div>
+                <div className="latest-item">
+                  <img
+                    src="http://dummyimage.com/64x64/cccccc/fff.jpg"
+                    alt=""
+                  />
+                  <a href="single-blog.html">
+                    <h6>Retro Art Party</h6>
+                  </a>
+                  <ul>
+                    <li>21 Sep,2015</li>
+                    <li>2 comments</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="gallery">
+                <h4>Gallery</h4>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="gallery-item">
+                  <img
+                    src="http://dummyimage.com/60x60/cccccc/fff.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div id="sub-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-12">
+              <p>
+                Copyrights 2015 <em>Auction</em>. Developed by Robert
+              </p>
+            </div>
+            <div className="col-md-6 col-sm-12">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Shop</a>
+                </li>
+                <li>
+                  <a href="#">Pages</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="#" className="go-top">
+        <i className="fa fa-angle-up" />
+      </a>
+    </>
+  );
+};
+
+export default Footer;
